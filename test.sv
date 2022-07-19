0d2b43d96bdbbf82d85113dc893c0db9741e59e3
Saleem Raza <95694625+RazaSaleem@users.noreply.github.com>
2022-07-19 10:03:58 +0500

    Create test.sv

---
+++ test.sv
@@ -0,0 +1,21 @@
+//A program block that creates the environment and initiate the stimulus
+`include "environment.sv"
+
+program test(dut_if vif);
+  
+  //declare environment handle
+  environment env;
+  
+  initial begin
+    //create environment
+    env = new(vif, 5);
+    
+    
+   // env.gen.total_trans = 5;
+    
+    //initiate the stimulus by calling run of env
+    env.run();
+
+  end
+
+endprogram
